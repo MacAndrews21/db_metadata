af3e214646b9120e7fffea5436e10b2b77773001
Andreas <walter_andreas_@gmx.de>
2015-01-19 00:24:46 +0100

    build connection to pg

--- py/functions.py
+++ py/functions.py
@@ -4,73 +4,20 @@ import os
 import sys
 import gdal
 from gdalconst import *
+import postgres as po
 
-dataset =gdal.Open("/home/andreas/Projekte/test-data/1012_1955.tif", GA_ReadOnly)
-width = dataset.RasterXSize
-height = dataset.RasterYSize
-#print 'Projection:', dataset.GetProjection()
-
-geotrans = dataset.GetGeoTransform()
-if not geotrans is None:
-    pixelSize = geotrans[1]
+##metaData = os.popen('gdalinfo -stats -nomd -norat -noct -nofl ' + filePath + fileName)
+def getResolution (filePath, fileName):
+    dataset = gdal.Open(filePath + fileName, GA_ReadOnly)
+    width = dataset.RasterXSize
+    height = dataset.RasterYSize
+    #print 'Projection:', dataset.GetProjection()
     
+    geotrans = dataset.GetGeoTransform()
+    if not geotrans is None:
+        pixelSize = geotrans[1]
     
-res = width * pixelSize
-resres = height * pixelSize
-print res, resres
-
-
-
-##def getResolution (filePath, fileName):
-    ##metaData = os.popen('gdalinfo -stats -nomd -norat -noct -nofl ' + filePath + fileName)
-    ##data = metaData.readlines()
-    ##metaData.close()
-    
-    ##def getBrackets(currentLine):
-        ##''' Select brackets '''
-        ##startBracket = currentLine.index("(") + 1
-        ##endBracket = currentLine.index(")", startBracket)            
-        ##brackets = currentLine[startBracket:endBracket]       
-        
-        ##''' get all before the comma and remove whitespace with .strip()'''
-        ##comma = brackets.index(",")
-        ##X = float(brackets[:comma].strip())
-        ##Y = float(brackets[comma + 1:].strip())
-        
-        ##''' return first and second value of the brackets'''
-        ##return X, Y
-    
-    
-    ##cornerCoordinates = {}
-    ###print data
-    ##for lines in range(len(data)):
-        ##line = data[lines]
-        ##if 'Pixel Size' in line:
-            ##pxSize, negPxSize = getBrackets(line)
-            ###pixelSize['positiv'] = pxSize
-            ###pixelSize['negativ'] = negPxSize
-            ##cornerCoordinates['pixelSize'] = pxSize
-            
-        ##if 'Size is' in line:
-            ##midle = line.rfind(',')
-            ##w = midle - 4
-            ##width = line[w:midle]
-            ##hs = midle + 2
-            ##h = hs + 4
-            ##height = line[hs:h]
-            ##print width, height
-            ###pixelSize['positiv'] = pxSize
-            ###pixelSize['negativ'] = negPxSize
-            ##cornerCoordinates['width'] = width
-            ##cornerCoordinates['height'] = height
-    ###print cornerCoordinates
-    ##test = float(cornerCoordinates['width']) / float(cornerCoordinates['pixelSize'])
-    ##print test
-    ##return cornerCoordinates
-
-
-
-
+    return width, height, pixelSize
 
 def createFileNameList(folderPath):
     
@@ -79,27 +26,35 @@ def createFileNameList(folderPath):
     
     
     
-    for filename in glob.glob(os.path.join(folderPath, '*.tif')):
+    for fileData in glob.glob(os.path.join(folderPath, '*.tif')):
         ''' create temporary dictionary '''
         namesDictionary = {}
         
         ''' seperate filename '''
-        startIndex = filename.rfind('/') + 1
-        name = filename[startIndex:]
+        startIndex = fileData.rfind('/') + 1
+        fileName = fileData[startIndex:]
+        #print fileName
+        
+        width, height, pixelSize = getResolution(folderPath, fileName)
 
         
         ''' get kb id '''        
-        kb = name[:4]
+        kb = fileName[:4]
         
         ''' get kb year '''        
-        end = name.rfind('.')
-        year = name[5:end]
+        end = fileName.rfind('.')
+        year = fileName[5:end]
         
-        ''' put name, id and year into temporary dictionary '''
-        namesDictionary["name"] = name
+        ''' put fileName, id and year into temporary dictionary '''
+        namesDictionary["name"] = fileName
         namesDictionary["id"] = kb
         namesDictionary["year"] = year
+        namesDictionary["width"] = width
+        namesDictionary["height"] = height
+        namesDictionary["pixelSize"] = pixelSize
 
         ''' add filename to list '''
         fileNameList.append(namesDictionary)
+        
+        
     return fileNameList
